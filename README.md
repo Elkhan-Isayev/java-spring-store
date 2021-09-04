0391e6032112a3987c69a13bde20898cac1015b4
Elkhan <is.elxan@gmail.com>
2021-09-04 22:30:47 +0400

    feature: init first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Welcome
+It's program that demonstrates connections between users and purchases.
+## Installation
+1. Download project to your computer
+2. Open project in the terminal
+3. Run docker-compose up -d
+4. Run project 
+
+## Can I contribute?
+
+Yes you can!  Contributions are welcomed. Thank you for considering contributing to the project!
---
+++ src/main/java/com/encom/springstore/SpringStoreApplication.java
@@ -0,0 +1,13 @@
+package com.encom.springstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringStoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringStoreApplication.class, args);
+    }
+
+}
