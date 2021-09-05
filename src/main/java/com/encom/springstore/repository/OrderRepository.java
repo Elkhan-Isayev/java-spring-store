8197fbf7e32417d68694fd6796cd06186395df59
Elkhan <is.elxan@gmail.com>
2021-09-05 16:50:57 +0400

    feature: add basic logic for orders

--- src/main/java/com/encom/springstore/repository/OrderRepository.java
+++ src/main/java/com/encom/springstore/repository/OrderRepository.java
@@ -1,4 +1,8 @@
 package com.encom.springstore.repository;
 
-public interface OrderRepository {
+import com.encom.springstore.model.entity.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OrderRepository extends JpaRepository<Order, Integer> {
+    // custom methods for specific queries
 }
